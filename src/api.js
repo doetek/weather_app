5a28c3e26d53bfe8b820bc1bb9d345f32f3aed9f
webguru12 <oyetundetobi1994@gmail.com>
2023-10-14 15:25:04 +0100

    version 1

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8dc29d5f7amsh9f6c0a0dbad300ap13e3bejsnafa652777d2d",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cb61bcfeb2dd7632f045c93a95debaef"; // enter your key from openweather API
